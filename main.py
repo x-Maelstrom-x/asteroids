7809e6a6eae88fe8d6edc0ee0fbd70238ce952f9
x-Maelstrom-x <edward.j.clack@gmail.com>
2025-11-19 17:39:02 +0000

    Player Hitbox

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     
     while True:
         log_state()
@@ -15,6 +17,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()

51fdb94f53f5acdac82038f56c4ecbb873f82ebe
x-Maelstrom-x <edward.j.clack@gmail.com>
2025-11-28 21:38:40 +0000

    Asteroid Splitting - Final Lesson

--- main.py
+++ main.py
@@ -42,6 +42,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
         screen.fill("black")  
         for drawables in drawable:
             drawables.draw(screen)
